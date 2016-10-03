ab237326e04a6c1653b135fcc1d7c9801794a8ee
Shawkath Khan <RLE0221@rlpl.com>
2016-10-03 21:12:54 +0530

    added d4d yaml

--- jobs/test2Jobs.groovy
+++ jobs/test2Jobs.groovy
@@ -4,7 +4,7 @@ import jenkins.model.*
 
 Yaml yml = new Yaml()
 		def build = Thread.currentThread().executable
-		def configyaml=build.workspace.toString()+"\\data\\bluenode.yaml";
+		def configyaml=build.workspace.toString()+"\\data\\d4d.yaml"; 
 
 		InputStream istream =new FileInputStream( new File(configyaml));
 		def jobSettings= yml.load(istream);
